878e3776ec0a78304e066ce6f57501de39818cc3
Maryel <maryem.benmassoud@esprit.tn>
2023-10-16 00:49:36 +0200

    2nd commit test

---
+++ src/test/java/tn/esprit/spring/kaddem/services/IUniversiteService.java
@@ -0,0 +1,2 @@
+package tn.esprit.spring.kaddem.services;public interface IUniversiteService {
+}
---
+++ src/test/java/tn/esprit/spring/kaddem/services/UniversiteServiceImplTest.java
@@ -0,0 +1,2 @@
+package tn.esprit.spring.kaddem.services;public class UniversiteServiceImplTest {
+}
