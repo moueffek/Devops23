6f9c77be66df633b4ef8cb5f92f83108af52f4f5
zeineb-eya <zeineb.rahmani@esprit.tn>
2023-10-16 01:01:36 +0100

    test: test Number of valid Contracts

--- src/test/java/tn/esprit/spring/kaddem/services/ContratServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/services/ContratServiceImplTest.java
@@ -5,8 +5,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.spring.kaddem.entities.Contrat;
+import tn.esprit.spring.kaddem.entities.Etudiant;
 import tn.esprit.spring.kaddem.entities.Specialite;
 import tn.esprit.spring.kaddem.repositories.ContratRepository;
+import tn.esprit.spring.kaddem.repositories.EtudiantRepository;
 
 import java.util.Date;
 import java.util.Optional;
@@ -20,6 +22,8 @@ class ContratServiceImplTest {
 
     @Mock
     ContratRepository contratRepository;
+    @Mock
+    EtudiantRepository etudiantRepository;
     @InjectMocks
     ContratServiceImpl contratService;
 
@@ -66,4 +70,52 @@ class ContratServiceImplTest {
         assertEquals(expectedContrat, retrievedContrat);
     }
 
+
+
+    @Test
+    @Order(4)
+    public void testNbContratsValides() {
+        Date startDate = new Date();
+        Date endDate = new Date();
+
+        int expectedValidContrats = 5;
+
+        when(contratRepository.getnbContratsValides(startDate, endDate)).thenReturn(expectedValidContrats);
+
+        Integer actualValidContrats = contratService.nbContratsValides(startDate, endDate);
+
+        verify(contratRepository, times(1)).getnbContratsValides(startDate, endDate);
+
+        assertEquals(expectedValidContrats, actualValidContrats);
+    }
+
+
+
+ /*   @Test
+    @Order(4)
+    public void testAffectContratToEtudiant() {
+        Specialite IA = Specialite.IA;
+        Contrat contrat = new Contrat(new Date(), new Date(), IA, false, 1);
+
+        contrat.setIdContrat(1);
+
+        Etudiant etudiant = new Etudiant();
+        etudiant.setNomE("Eya");
+        etudiant.setPrenomE("Rahmani");
+
+        when(etudiantRepository.findByNomEAndPrenomE("Eya", "Rahmani")).thenReturn(etudiant);
+
+        when(contratRepository.findByIdContrat(1)).thenReturn(contrat);
+
+        Contrat affectedContrat = contratService.affectContratToEtudiant(1, "Eya", "Rahmani");
+
+        verify(etudiantRepository, times(1)).findByNomEAndPrenomE("Eya", "Rahmani");
+        verify(contratRepository, times(1)).findByIdContrat(1);
+
+        assertNotNull(affectedContrat);
+
+        if (affectedContrat != null) {
+            assertEquals(etudiant, affectedContrat.getEtudiant());
+        }
+    }*/
 }
