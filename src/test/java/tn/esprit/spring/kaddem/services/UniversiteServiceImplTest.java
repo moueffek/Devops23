413841374c57610a56cc35b90225be6f0c828ee6
Maryel <maryem.benmassoud@esprit.tn>
2023-10-21 18:57:20 +0200

    updates

--- src/test/java/tn/esprit/spring/kaddem/services/UniversiteServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/services/UniversiteServiceImplTest.java
@@ -11,7 +11,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.spring.kaddem.entities.Universite;
 import tn.esprit.spring.kaddem.repositories.UniversiteRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -30,17 +35,33 @@ public class UniversiteServiceImplTest {
     public void testRetrieveUniversiteTest() {
 
        Universite universite = new Universite(1, "Université1");
-
         universite.setIdUniv(1);
-
         when(universiteRepository.findById(1)).thenReturn(Optional.of(universite));
 
         universiteService.retrieveUniversite(1);
         Assertions.assertNotNull(universite);
 
         System.out.println(universite);
-        System.out.println(" University Retrieve processed succefully...!!");
+        System.out.println("University Retrieve processed succefully...!!");
 
+    }
+
+    @Test
+    public void testRetrieveAllUniversiteTest()
+    {
+        List<Universite> Universitelist = new ArrayList<Universite>() {
+
+            {
+                add(new Universite(2, "Université2"));
+                add(new Universite(3, "Université3"));
+                add(new Universite(4, "Université4"));
+                add(new Universite(5, "Université5"));
+
+            }};
+        when(universiteService.retrieveAllUniversites()).thenReturn(Universitelist);
+        List<Universite> universitelist = universiteService.retrieveAllUniversites();
+        assertEquals(4, universitelist.size());
+        System.out.println("All University Retrieve processed succefully...!!");
     }
 
 
