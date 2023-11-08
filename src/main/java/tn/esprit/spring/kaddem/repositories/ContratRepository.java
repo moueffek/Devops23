25b3de242bb0b41fdda2f750a34f0bc3893e723f
Maryel <maryem.benmassoud@esprit.tn>
2023-11-08 17:01:10 +0100

    update

--- src/main/java/tn/esprit/spring/kaddem/repositories/ContratRepository.java
+++ src/main/java/tn/esprit/spring/kaddem/repositories/ContratRepository.java
@@ -1,10 +1,5 @@
 package tn.esprit.spring.kaddem.repositories;
 
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.FluentQuery;
